e9675508bad08d6c144119f6f5846fc178f12ffa
gi <ibragi_guzel@mail.ru>
2021-01-22 11:04:12 +0300

    реализована функция add

---
+++ library_1.py
@@ -0,0 +1,25 @@
+lib = {
+            "title": "Белый дракон (роман)",
+            "year": 2013,
+            "pages": 547,
+            "isbn13": 978-1-139-33287-3,
+            "rating": 4,
+            "price": 1500,
+            "discount": 10,
+            "author": 'Александр Стула'
+        }
+
+
+def add(lib):
+    lib['book'] = "Отцы и дети"
+    print(lib)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    add(lib)
+
